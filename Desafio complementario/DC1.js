0ac59b899f81caecbaf0d6b4d1795e8d08046173
Santiagocast <62676315+Santiagocast@users.noreply.github.com>
2022-02-17 17:50:50 -0300

    DC1 aprobado

--- Desafio complementario/DC1.js
+++ Desafio complementario/DC1.js
@@ -1,2 +1,12 @@
-let variable1 = prompt("INGRESE UN NUMERO");
-console.log(variable1);
+function pedirNumero (vez){
+let numero = parseInt(prompt("Ingrese el numero " + vez));
+return numero;
+}
+let suma = 0;
+let cantidad = prompt("Ingrese cantidad de numeros a sumar");
+for(let i= 1; i-1<cantidad; i++){
+    let numActual = pedirNumero(i);
+    suma += numActual;
+}
+console.log(suma);
+alert("La suma es igual a: " + suma)
