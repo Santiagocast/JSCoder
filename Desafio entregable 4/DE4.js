e3aa42e7f65e8f2d3a218a725c1092855398f8e9
Santiagocast <santiagojcastelli@gmail.com>
2022-03-15 00:44:12 -0300

    Agrego boton eliminar grupo

--- Desafio entregable 4/DE4.js
+++ Desafio entregable 4/DE4.js
@@ -251,9 +251,42 @@ function agregarGrupo(){
 function actualizarDetallesGrupo(grupo, indiceEnStorage){
     let lugarAgregar = document.getElementById("gruposNuevitos");
     let listaNueva = document.createElement("li");
-    listaNueva.innerHTML =`<a class="nav-link" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text" aria-hidden="true"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>${grupo.nombre}</a>`;
-    listaNueva.id = "grupo" + indiceEnStorage;
-    lugarAgregar.append(listaNueva);
+    let divPrincipal = document.createElement("div");
+    let divGrupo = document.createElement("div");
+    let divBasura = document.createElement("div");
+    let nombreGrupo = document.createElement("a");
+    let basura = document.createElement("a");
+    let iconoGrupo = document.createElement("svg")
+    let iconoBasura = document.createElement("svg")
+    divPrincipal.className = "d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-0 pb-0 mb-0"
+    divGrupo.className = "btn-toolbar mb-2 mb-md-0";
+    divBasura.className = "btn-toolbar mb-2 mb-md-0";
+    nombreGrupo.href = "#";
+    basura.href = "#";
+    nombreGrupo.className = "nav-link";
+    basura.className = "nav-link";
+    basura.id = "basura" + indiceEnStorage;
+    nombreGrupo.id = "grupo" + indiceEnStorage;
+    iconoGrupo.innerHTML = 
+        `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16">
+            <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+            <path fill-rule="evenodd" d="M5.216 14A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216z"/>
+            <path d="M4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z"/>
+        </svg> ${grupo.nombre}`;
+    iconoBasura.innerHTML =
+        `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>
+            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>
+        </svg>`;
+    //Agrego la lista de grupo.
+    nombreGrupo.appendChild(iconoGrupo);
+    basura.appendChild(iconoBasura);
+    divGrupo.appendChild(nombreGrupo);
+    divBasura.appendChild(basura);
+    divPrincipal.appendChild(divGrupo);
+    divPrincipal.appendChild(divBasura);
+    listaNueva.appendChild(divPrincipal);
+    lugarAgregar.appendChild(listaNueva);
     mostrarDatosGrupoPantallaPrincipal(grupo);
     actualizarTarjetas(indiceEnStorage-1)
 }
